a47d591851bb08d5c7dc1d2dcd7a98fd10352af6
tomaszuk <daniel.tomaszuk@netguru.com>
2021-01-10 11:26:38 +0100

    feature. Add health endpoint.

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from app.api.endpoints import health
 
 api_router = APIRouter()
-# api_router.include_router(health.router, tags=["health"])
+api_router.include_router(health.router, tags=["health"])
